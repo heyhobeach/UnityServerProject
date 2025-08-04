b890a4c3211686b137d1c1b42fa249034df0f23f
heyhobeach <72774909+heyhobeach@users.noreply.github.com>
2025-08-05 00:29:01 +0900

    Add minimal API endpoints and solution file
    Introduced basic CRUD endpoints for Todo items in Program.cs using minimal APIs. Added missing package reference for EntityFramework diagnostics. Created a new Visual Studio solution file for the project.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Diagnostics.ent
 using UnityServerProject.Data;
+using UnityServerProject.Model;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +17,14 @@ builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 //builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.MapGet("/todoitems", async (TodoDb db) => await db.Todos.ToListAsync());
+app.MapGet("/todoitems/complete", async (TodoDb db) => await db.Todos.Where(t => t.IsComplete).ToListAsync());
+app.MapGet("/todoitems/{id}", async (int id, TodoDb db) => await db.Todos.FindAsync(id) is Todo todo ? Results.Ok(todo) : Results.NotFound());
+app.MapPost("/todoitems", async (Todo todo, TodoDb db) => { 
+    db.Todos.Add(todo);
+    await db.SaveChangesAsync();
+    return Results.Created($"/todoitems/{todo.Id}", todo);
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
