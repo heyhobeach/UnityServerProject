17186abf534134714fb26962676af4b1abbc923d
heyhobeach <zxc120021@gmail.com>
2025-08-04 23:54:19 +0900

    Add Todo model and in-memory database setup
    Introduced the Todo entity and TodoDb context for managing todos using Entity Framework Core. Configured the application to use an in-memory database for Todo items and added the necessary EF Core package.

---
+++ Data/TodoDb.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+namespace UnityServerProject.Data
+{
+    public class TodoDb:DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options):base(options)
+        {
+        }
+
+        public DbSet<Model.Todo> Todos => Set<Model.Todo>();
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Diagnostics.ent
+using UnityServerProject.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
+//useinmemoryDatabase가 나중에 배포시에도 사용해도 되는지 메모리에 저장되는거라면 안 될거같은 느낌임
+builder.Services.AddDbContext<TodoDb>(opt => opt.UseInMemoryDatabase("TodoList"));
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+//자습서에서 현재 안 쓰는듯?
+//builder.Services.AddControllers();
+//// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+//builder.Services.AddEndpointsApiExplorer();
+//builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
